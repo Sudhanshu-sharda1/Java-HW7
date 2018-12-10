270eb6c1b1c30fbacb81d9ba4203f9fae14c92a6
Sudhanshu Sharda <sudhanshu.sharda@yahoo.com>
2018-12-10 10:47:42 +0530

    Student Grade

---
+++ TestDetails.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+public class TestDetails {
+
+	String studentName[]=new String[5];
+	char grade[]=new char[5];
+	double studentMark[][]=new double[5][4];
+
+	 void details()
+	{
+		Scanner input= new Scanner(System.in);
+		for(int i=0;i<5;i++) {
+			System.out.println("Enter Student Name:");
+			studentName[i]=input.next();
+		}
+		for(int i=0;i<5;i++) {
+			System.out.println("Enter the marks:");
+			for(int j=0;j<4;j++) {
+				studentMark[i][j]=new 
+			}
+			
+		}
+		
+	}
+	static double Average()
+	{
+		for(int i=0;i<5;i++) {
+		
+		}
+	}
+}
